f0d1bd2d73ce4420519cdf7bab5b6958ec217366
CaitlinRC <caitlinburns2308@gmail.com>
2018-03-14 16:04:13 +0000

    Java Files now read the file and allow you to answer questions

--- runQuiz.java
+++ runQuiz.java
@@ -2,6 +2,6 @@ public class runQuiz {
 
   public static void main(String args[]) {
 
-    new takeQuiz(args[0]);
+    new takeQuiz("quiz.txt");
   }
 }
--- takeQuiz.java
+++ takeQuiz.java
@@ -4,25 +4,69 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
 import java.io.FileNotFoundException;
+import java.util.*;
 
 public class takeQuiz {
 
   public takeQuiz(String fileName) {
-    
+
     try {
       File fileIn = new File(fileName);
       Scanner in = new Scanner(fileIn);;
 
+      String question;
+      ArrayList correctAnswers = new ArrayList();
+      int questionNumber = 0;
 
     while (in.hasNextLine()) {
+      // Maybe do text file with stop words?
       String line = in.nextLine();
+      String testEmpty = line.replaceAll("\"", "");
 
-      System.out.println(line);
+      if (testEmpty.isEmpty() == false) {
+        printValues(line, correctAnswers);
     }
+      else {
+        getAnswer(correctAnswers, questionNumber);
+        questionNumber += 1;
+      }
   }
+  getAnswer(correctAnswers, questionNumber);
+}
 
     catch (FileNotFoundException e){
       System.out.println("Problem");
+    }
+  }
+
+  public void getAnswer(ArrayList correctAnswers, int questionNumber) {
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("Enter your answer: ");
+    String userAnswer = userInput.nextLine();
+
+    userAnswer = userAnswer.replaceAll("\\pP", "");
+
+    if (correctAnswers.indexOf(userAnswer) == questionNumber) {
+      System.out.println("Correct!");
+    }
+
+    else {
+      System.out.println("Incorrect!");
+    }
+
+  }
+
+  public void printValues(String line, ArrayList correctAnswers) {
+
+    if (line.contains("*")) {
+      System.out.println(line.replaceAll("\\pP", "")); // strips punctuation
+
+      if (line.contains("-")) {
+        correctAnswers.add(line.replaceAll("\\pP", ""));
+      }
+    }
+    else {
+      System.out.println(line);
     }
   }
 }
